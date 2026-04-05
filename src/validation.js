ee6d7a8abae5dcfa6ac6479c6f60578ec24c10c7
tcpstrah <kuznetsov-a-v2007@yandex.ru>
2026-04-05 11:52:53 +0000

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -5,6 +5,11 @@ function validateEmail(email) {
 
 module.exports = { validateEmail };
 
+<<<<<<< HEAD
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
+=======
+function validatePassword(password) {
+    return password.length >= 8;
+>>>>>>> 2b03f48 (Add password validation)
 }
