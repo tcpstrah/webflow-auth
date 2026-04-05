1cc29ff37b84356e54a68e2a25830b029c9d182c
tcpstrah <kuznetsov-a-v2007@yandex.ru>
2026-04-05 11:50:43 +0000

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,21 @@ function login(username, password) {
 }
 
 module.exports = { login };
-function checkCredentals(username, password) {  // опечатка в названии
+function checkCredentials(username, password) {  // опечатка в названии
     return username && password;
 }
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+function login(username, password) {
+    if (checkCredentials(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
+}
+>>>>>>> 2168092 (Implement login function)
+=======
+
+console.log('Debug: auth module loaded');  // отладочный код
+>>>>>>> 955b115 (WIP: debugging)
