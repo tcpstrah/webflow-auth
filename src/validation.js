61ce8485af23e791bd985551aaf86f6ccaceb4d2
tcpstrah <kuznetsov-a-v2007@yandex.ru>
2026-04-05 11:57:55 +0000

    Critical security patch: use HTTPS and add input sanitization

--- src/validation.js
+++ src/validation.js
@@ -6,10 +6,19 @@ function validateEmail(email) {
 module.exports = { validateEmail };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
 =======
 function validatePassword(password) {
     return password.length >= 8;
 >>>>>>> 2b03f48 (Add password validation)
+=======
+function validatePassword(password) {
+    return password.length >= 8;
+=======
+function sanitizeInput(input) {
+    return input.replace(/[<>]/g, '');  // защита от XSS
+>>>>>>> 9af46aa (Critical security patch: use HTTPS and add input sanitization)
+>>>>>>> f966cdb (Critical security patch: use HTTPS and add input sanitization)
 }
